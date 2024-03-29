c11e363ba74d429525d31aa822cc499061babfc2
Mikaël Sévigny <sevignator@hotmail.com>
2024-03-29 08:46:41 -0400

    Add simple EventEmitter demo

---
+++ 02-eventemitter/eventemitter-demo/app.js
@@ -0,0 +1,27 @@
+const EventEmitter = require('events');
+
+class Emitter extends EventEmitter {}
+
+const myEmitter = new Emitter();
+
+myEmitter.on('foo', () => {
+  console.log('foo #1 occurs');
+});
+
+myEmitter.once('foo', () => {
+  console.log('foo #2 occurs (once)');
+});
+
+myEmitter.on('foo', () => {
+  console.log('foo #3 occurs');
+});
+
+myEmitter.on('bar', () => {
+  console.log('bar #1 occurs');
+});
+
+for (let i = 0; i < 2; i++) {
+  myEmitter.emit('foo');
+  myEmitter.emit('bar');
+  console.log('---------------');
+}
