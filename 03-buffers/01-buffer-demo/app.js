09101206e6e89470f7ed162b7fb0fd4469e2be1a
Mikaël Sévigny <sevignator@hotmail.com>
2024-04-04 15:36:37 -0400

    Improve buffer demo

--- 03-buffers/01-buffer-demo/app.js
+++ 03-buffers/01-buffer-demo/app.js
@@ -1,5 +1,15 @@
 import { Buffer } from 'node:buffer';
 
-const memoryContainer = Buffer.from('4c616d70', 'hex');
+const memoryContainer1 = Buffer.alloc(5);
+memoryContainer1[0] = 0x48;
+memoryContainer1[1] = 0x65;
+memoryContainer1[2] = 0x6c;
+memoryContainer1[3] = 0x6c;
+memoryContainer1[4] = 0x6f;
 
-console.log(memoryContainer.toString('ascii')); // Lamp
+const memoryContainer2 = Buffer.from('576f726c64', 'hex');
+
+const hiddenWord1 = memoryContainer1.toString('ascii');
+const hiddenWord2 = memoryContainer2.toString('ascii');
+
+console.log(hiddenWord1, hiddenWord2); // Hello World
