f30c8f1c444d9c8b3b0be1072bfaba5c0d514474
Mikaël Sévigny <sevignator@hotmail.com>
2024-04-12 15:35:02 -0400

    Add custom event event and Nodemon package

--- projects/file-commander/app.js
+++ projects/file-commander/app.js
@@ -1,11 +1,25 @@
-import fs from 'node:fs/promises';
+const fs = require('fs/promises');
 
 (async () => {
-  const watcher = fs.watch('./command.txt')
+  const commandFileHandler = await fs.open('./command.txt', 'r');
+  const watcher = fs.watch('./command.txt');
+
+  commandFileHandler.on('change', async () => {
+    const fileSize = (await commandFileHandler.stat()).size;
+    const buffer = Buffer.alloc(fileSize);
+    const content = await commandFileHandler.read({
+      buffer,
+      offset: 0,
+      length: buffer.byteLength,
+      position: 0,
+    });
+
+    console.log(content);
+  });
 
   for await (const event of watcher) {
     if (event.eventType === 'change') {
-      console.log('File changed');
+      commandFileHandler.emit('change');
     }
   }
 })();
